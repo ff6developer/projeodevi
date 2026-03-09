b3e2caac5e4fe2df11c345bf5d30d7b738b39b8b
Ad Soyad <mailin@gmail.com>
2026-03-09 17:16:24 +0300

    güncelleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,23 @@
-import "./index.css";
 
+import React from "react";
+import "./index.css";
+import Header from "./components/header.jsx";
 function App() {
   return (
     <div className="layout">
 
-      <header className="header">
-        <div className="header-left">
-          <img src="/logo.png" alt="logo" className="logo" />
-          <h1 className="logo-text">ELMENES COFFEE</h1>
-        </div>
+      <Header />
 
-        <nav className="nav">
+      <div className="main">
+        <aside className="sidebar">
           <ul>
-            <li><a href="#">coffee Arenası</a></li>
+            <li><a href="#">Kahve Arenası</a></li>
             <li><a href="#">Hakkımızda</a></li>
             <li><a href="#">Menü</a></li>
             <li><a href="#">Kahveni Tasarla</a></li>
             <li><a href="#">Kampanyalar</a></li>
             <li><a href="#">Kayıt Ol</a></li>
             <li><a href="#">Giriş Yap</a></li>
-          </ul>
-        </nav>
-      </header>
-
-      <div className="main">
-        <aside className="sidebar">
-          <ul>
-            <li className="active"><a href="#">Kahve Arenası</a></li>
-            <li className="active"><a href="#">Hakkımızda</a></li>
-            <li className="active"><a href="#">Menü</a></li>
-            <li className="active"><a href="#">Kahveni Tasarla</a></li>
-            <li className="active"><a href="#">Kampanyalar</a></li>
-            <li className="active"><a href="#">Kayıt Ol</a></li>
-            <li className="active"><a href="#">Giriş Yap</a></li>
           </ul>
         </aside>
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+<nav className="nav">
+          <ul>
+            <li><a href="#">Kahve Arenası</a></li>
+            <li><a href="#">Hakkımızda</a></li>
+            <li><a href="#">Menü</a></li>
+            <li><a href="#">Kahveni Tasarla</a></li>
+            <li><a href="#">Kampanyalar</a></li>
+            <li><a href="#">Kayıt Ol</a></li>
+            <li><a href="#">Giriş Yap</a></li>
+          </ul>
+        </nav>
